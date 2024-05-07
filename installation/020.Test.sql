593b8301871c1aeb55bc8dad2e2749af4f3ca5a5
Vadim Glebov <glebowadim@gmail.com>
2024-05-07 22:37:56 +0700

    Test password param

--- installation/020.Test.sql
+++ installation/020.Test.sql
@@ -1,13 +1,4 @@
-declare
-    v_result varchar2(4000);
-    v_count number;
 begin
     dbms_output.put_line(util_module.get_module_param_val('test_repo', 'Token') || ' - encrypt');
     dbms_output.put_line(util_module.get_decrypted_module_param_val('test_repo', 'Token') || ' - decrypt');
-
-    select count(*)
-      into v_count
-      from module_param_temp;
-    dbms_output.put_line(v_count);
-
 end;
