394b42e74bd3a505c363b2cdf99e0353d331d857
Vadim Glebov <glebowadim@gmail.com>
2024-05-07 19:16:07 +0700

    Test password param

--- installation/020.Test.sql
+++ installation/020.Test.sql
@@ -1,4 +1,4 @@
 begin
-    dbms_output.put_line(util_module.get_module_param_val('test_repo', 'Token'));
-    dbms_output.put_line(util_module.get_decrypted_module_param_val('test_repo', 'Token'));
+    dbms_output.put_line(util_module.get_module_param_val('test_repo', 'Token') || ' - encrypt');
+    dbms_output.put_line(util_module.get_decrypted_module_param_val('test_repo', 'Token') || ' - decrypt');
 end;
