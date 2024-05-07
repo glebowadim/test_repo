455228fefdcb42bb52268ed036492b258c6f4ac3
Vadim Glebov <glebowadim@gmail.com>
2024-05-07 23:19:56 +0700

    Test password param

--- installation/020.Test.sql
+++ installation/020.Test.sql
@@ -1,4 +1,13 @@
+declare
+    v_result varchar2(4000);
 begin
     dbms_output.put_line(util_module.get_module_param_val('test_repo', 'Token') || ' - encrypt');
     dbms_output.put_line(util_module.get_decrypted_module_param_val('test_repo', 'Token') || ' - decrypt');
+
+    select version
+      into v_result
+      from module
+     where module_name = 'test000';
+    dbms_output.put_line(v_result);
+
 end;
